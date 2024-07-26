add6bf7b34659aa460af988f527955846376c4dd
Carlos Vallory <carlvallory@gmail.com>
2024-07-26 16:25:48 -0400

    minor fixes 1.0.14

--- resources/views/frontend/payment/pagopar.blade.php
+++ resources/views/frontend/payment/pagopar.blade.php
@@ -44,23 +44,23 @@
                 <div class="row">
                   <div class="col-md-6">
                     <h3>Billing Address</h3>
-                    <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-                    <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+                    <label for="fname"><i class="fa fa-user"></i> Nombre completo</label>
+                    <input type="text" id="fname" name="firstname" placeholder="John M. Doe" value="{{ $data['shipping_name'] }}">
                     <label for="email"><i class="fa fa-envelope"></i> Email</label>
-                    <input type="text" id="email" name="email" placeholder="john@example.com">
-                    <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-                    <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-                    <label for="city"><i class="fa fa-institution"></i> City</label>
+                    <input type="text" id="email" name="email" placeholder="john@example.com" value="{{ $data['shipping_email'] }}">
+                    <label for="adr"><i class="fa fa-address-card-o"></i> Dirección </label>
+                    <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" value="{{ $data['shipping_address'] }}">
+                    <label for="city"><i class="fa fa-institution"></i> Ciudad</label>
                     <input type="text" id="city" name="city" placeholder="New York">
 
                     <div class="row">
                       <div class="col-md-6">
-                        <label for="state">State</label>
+                        <label for="state">Departamento</label>
                         <input type="text" id="state" name="state" placeholder="NY">
                       </div>
                       <div class="col-md-6">
                         <label for="zip">Zip</label>
-                        <input type="text" id="zip" name="zip" placeholder="10001">
+                        <input type="text" id="zip" name="zip" placeholder="10001" value="{{ $data['shipping_postCode'] }}">
                       </div>
                     </div>
                   </div>
